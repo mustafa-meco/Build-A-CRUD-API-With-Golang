2a1d42c46309dfd37b063d42617f76108020bafb
Mustafa <musgamal05@gmail.com>
2022-04-26 15:29:17 +0200

    repaired id error in endpoints

--- go-movies-crud/main.go
+++ go-movies-crud/main.go
@@ -100,10 +100,10 @@ func main() {
 	movies = append(movies, Movie{ID: "2", Isbn: "45455", Title: "Movie Two", Director : &Director{Firstname: "Steve", Lastname: "Smith"}})
 
 	r.HandleFunc("/movies", getMovies).Methods("GET")
-	r.HandleFunc("/movies/(id)", getMovie).Methods("GET")
+	r.HandleFunc("/movies/{id}", getMovie).Methods("GET")
 	r.HandleFunc("/movies", createMovie).Methods("POST")
-	r.HandleFunc("/movies/(id)", updateMovie).Methods("PUT")
-	r.HandleFunc("/movies/(id)", deleteMovie).Methods("DELETE")
+	r.HandleFunc("/movies/{id}", updateMovie).Methods("PUT")
+	r.HandleFunc("/movies/{id}", deleteMovie).Methods("DELETE")
 
 	fmt.Printf("Starting server at port 8000 \n")
 	log.Fatal(http.ListenAndServe(":8000",r))
